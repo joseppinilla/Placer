3da8889519e3bdc4155e3e93dd873e31105524bf
joseppinilla <joseppinilla@outlook.com>
2015-02-20 04:32:58 -0800

    Drawing random placement to start annealing.
    TODO: Cost Function

--- placerA2/placerA2.py
+++ placerA2/placerA2.py
@@ -1,6 +1,8 @@
 import time
 import sys
 import getopt
+import placerGUI
+import random
 import numpy as np
 import Tkinter as tk
 import networkx as nx
@@ -38,18 +40,31 @@ class Placer():
             sys.exit(2)
                
         
-        #=================Parse file to create graph=================#
+        #=============Parse file to create cells graph===============#
         self.G=nx.Graph()
         fin = open(inputfile,'r')
         self.getGraph(fin)
         fin.close() 
         
+        
+        #================Create Data Structures================# 
+        # Array of Line objects to draw connections
+        self.connLines = []
+        # Array of Block objects drawing the rectangles for each site on the circuit, tracks occupancy
+        self.sites = []
+        # Array of Text objects noting the name of the node assigned to a cell site 
+        self.tags = []
+        
+        #================Draw Buttons and plots================#
         self.master = master
         self.initialize_buttons()
         self.initialize_plots()
+        
+        #===============Start conditions================#
         self.initialize_start()
 
     def initialize_buttons(self):
+        """"""
         self.start_button = tk.Button(self.master, text='start', command = self.startrecording)
         self.start_button.grid(row=0, column=0)
 
@@ -61,6 +76,7 @@ class Placer():
 
 
     def initialize_start(self):
+        """"""
         self.stop_button['state'] = 'disabled'
         self.clear_button['state'] = 'disabled'  
 
@@ -71,17 +87,41 @@ class Placer():
         return np.exp(-t) * np.cos(2*np.pi*t)
 
     def initialize_plots(self):
-        print "init plot start"
+        """"""
+        # Draw circuit canvas with hard coded width 600 and adjustable height to circuit input
+        ckt_max_x = 600
+        ckt_max_y = (ckt_max_x*(self.rows))/self.cols
+        scale_x = round(ckt_max_x / self.cols)
+        scale_y = round(ckt_max_y / self.rows)
         
-        t1 = np.arange(0.0, 5.0, 0.1)
-        t2 = np.arange(0.0, 5.0, 0.02)
+        self.canvasCirkt = tk.Canvas(self.master,width=ckt_max_x,height=(ckt_max_y*2)+int(scale_y))
+        self.canvasCirkt.grid(row=1,column=0,columnspan=2)
         
-        # initialize figure        
-        self.figure, self.axes = plt.subplots(2,figsize=(4,8))
+        # Draw border
+        self.canvasCirkt.create_rectangle(1, 1, ckt_max_x, (ckt_max_y*2)+int(scale_y))
+        
+        #====Draw cell rows spaced by routing channels=====#
+        
+        for cut in range(int(scale_y), int(ckt_max_y*2), int(scale_y)*2):
+            for cut2 in range(1, int(ckt_max_x), int(scale_x)):
+                # Coordinates for top and bottom points of rectangle
+                points = (cut2, cut, cut2+scale_x-1, cut+scale_y)
+                blockObj = placerGUI.Block(self.canvasCirkt,points)
+                self.sites.append(blockObj)
+                
+        print "NUMBER OF SITES ", len(self.sites)
+        # Draw Figure for 2 subplots (Connections Graph and Cost Function)        
+        self.figure, self.axes = plt.subplots(2)
+        self.figure.set_figwidth(4)
         self.axGraph = self.axes[0]
         self.axCost = self.axes[1]
-        
+                
+        # Draw connection Graph
         nx.draw(self.G, ax=self.axGraph, with_labels=True)
+        
+        # Initialize cost function plot
+        t1 = np.arange(0.0, 5.0, 0.1)
+        t2 = np.arange(0.0, 5.0, 0.02)
                 
         plt.sca(self.axCost)
         plt.plot(t1, self.f(t1), 'bo', t2, self.f(t2), 'k')        
@@ -89,28 +129,6 @@ class Placer():
         self.canvasPlot = FigureCanvasTkAgg(self.figure, master=self.master)
         self.canvasPlot.get_tk_widget().grid(row=1,column=2)
         
-        ckt_max_x = 800
-        self.canvasCirkt = tk.Canvas(self.master,width=ckt_max_x)
-        self.canvasCirkt.grid(row=1,column=0,columnspan=2)
-        
-        
-        ckt_max_y = (ckt_max_x*(self.rows*2))/self.cols
-        scale_x = round(ckt_max_x / self.cols)
-        scale_y = round(ckt_max_y / self.rows)
-        
-        #for cut in range(int(scale_y), int(win_max_y + 1), int(scale_y)):
-    
-         #   for cut2 in range(int(scale_x), int(win_max_x + 1), int(scale_x)):
-        
-          #      point1 = graphics.Point(cut2, cut)
-           #     point2 = graphics.Point(cut2 - scale_x, cut - scale_y)           
-            #    blockObj = Block(win,point1,point2)
-             #   blocks.append(blockObj)
-              #  blockIndex+=1
-
-        
-
-
     def clear_all(self):
         plt.clf()
         self.initialize_plot()
@@ -134,19 +152,9 @@ class Placer():
     def quitApp(self):
         self.master.destroy()
         self.master.quit()
-        
-    def _startplacement(self):
-        
-        print "HERE" #TODO: Placement algorithm   
-        time.sleep(2)
-        self.axGraph.cla()
-        self.G.remove_node(0)
-        nx.draw(self.G, ax=self.axGraph, with_labels=True)
-        self.canvasPlot.draw()
-        self.canvasPlot.flush_events()            
-    
-    def getGraph(self, file):
-        tmpList = file.readline().split()
+          
+    def getGraph(self, fin):
+        tmpList = fin.readline().split()
         self.cells = int(tmpList[0])
         self.conns = int(tmpList[1])
         self.rows =  int(tmpList[2])
@@ -157,11 +165,68 @@ class Placer():
         self.G.add_nodes_from(range(0,self.cells))
         
         for conn in range(0,self.conns):
-            tmpList = file.readline().split()
+            tmpList = fin.readline().split()
             numBlocks = int(tmpList[0])
             srcBlock = int(tmpList[1])
+            self.G.node[srcBlock]["net"] = conn
             for block in range(2,numBlocks+1):
-                self.G.add_edge(srcBlock, int(tmpList[block]))   
+                sinkBlock = int(tmpList[block])
+                self.G.node[sinkBlock]["net"] = conn               
+                self.G.add_edge(srcBlock, sinkBlock)   
+
+
+    
+    def _startplacement(self):
+        
+        self.randPlace()
+        
+#         print "HERE" #TODO: Placement algorithm   
+#         time.sleep(2)
+#         self.axGraph.cla()
+#         self.G.remove_node(0)
+#         nx.draw(self.G, ax=self.axGraph, with_labels=True)
+#         self.canvasPlot.draw()
+#         self.canvasPlot.flush_events()            
+
+
+    def randPlace(self):
+        random.seed(30) #TODO: Adjustable Seed
+        sitesNum = self.rows*self.cols-1
+        for node in self.G.nodes():
+            randSite = random.randint(0,sitesNum)
+            while (self.sites[randSite].isOcp()):
+                randSite = random.randint(0,self.cells)    
+            
+            self.sites[randSite].setCell(node)
+            self.G.node[node]["site"] = self.sites[randSite]
+            siteX, siteY = self.G.node[node]["site"].getCenter()
+            self.tags.append(self.canvasCirkt.create_text(siteX, siteY, text=node)) 
+                        
+        self.drawConns()
+            
+    def drawConns(self):
+        """ Extract center point from each node and draw connection to other nodes """
+        #TODO: Draw each connection [once]
+        for node in self.G.nodes():
+                       
+            pX,pY = self.G.node[node]["site"].getCenter()
+            for nb in self.G.neighbors(node):
+                nbX,nbY = self.G.node[nb]["site"].getCenter()
+                self.connLines.append(self.canvasCirkt.create_line(pX,pY,nbX,nbY))
+                
+            
+            self.canvasCirkt.update()
+            
+            
+    def delConns(self):
+        
+        for line in self.connLines:
+            self.canvasCirkt.delete(line)    
+        
+            
+        
+            
+    
 
 
 
---
+++ placerA2/placerGUI.py
@@ -0,0 +1,38 @@
+import Tkinter
+
+
+"""Based on the graphics.py module"""
+class Block():
+
+    stateDict = {'free':0,'ocp':-1}
+    
+    def __init__(self,canvas,p):
+        
+        self.state = 0
+        self.cell = 0
+        self.p1X = (p[0])
+        self.p1Y = (p[1])
+        self.p2X = (p[2])
+        self.p2Y = (p[3])
+        canvas.create_rectangle(*p, fill="white")
+        
+    def setCell(self,cell):
+        self.state = self.stateDict["ocp"]
+        self.cell = cell
+        
+    def getCell(self):
+        return self.cell
+
+    def getCenter(self):
+        return (self.p1X+self.p2X)/2.0, (self.p1Y+self.p2Y)/2.0
+                
+    def isFree(self):
+        return (self.state == self.stateDict['free'])
+    
+    def isOcp(self):
+        return (self.state == self.stateDict['ocp'])
+    
+#     def setTag(self,win,tag):
+#         t = graphics.Text(self.getCenter(),tag) 
+#         t.draw(win)
+#         tList.append(t)
